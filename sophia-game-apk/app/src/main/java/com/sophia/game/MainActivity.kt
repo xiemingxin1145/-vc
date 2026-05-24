bf3c8d34d9551bf7fd865585f4829834b02a4dca
xiemingxin1145 <gayakethanda@gmail.com>
2026-05-25 06:56:39 +0800

    add MainActivity with WebView for lewd game

---
+++ sophia-game-apk/app/src/main/java/com/sophia/game/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.sophia.game
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+        webSettings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
+        webView.webViewClient = WebViewClient()
+
+        // 加载我们的色色游戏
+        // 目前先加载 GitHub Raw，后续可以改成本地 assets
+        webView.loadUrl("https://raw.githubusercontent.com/xiemingxin1145/-vc/main/games/sophia-private-course/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
